74d95e4ef1a80f034456d32805d711806e2a06c8
Mielesgames <emieltlam11@gmail.com>
2023-06-19 12:21:50 +0200

    Spotify API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomePageComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "login", component: LoginPageComponent},
+  {path: "**", component: HomePageComponent} //replace with 404page component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
