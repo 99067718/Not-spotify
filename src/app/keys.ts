56f9859d7dcbe5ffff4a758fb9771474dcf393c8
Mielesgames <emieltlam11@gmail.com>
2023-06-20 11:37:25 +0200

    oh no, the keys.ts file is now hidden, sad isn't it?

--- src/app/keys.ts
+++
@@ -1,3 +0,0 @@
-// import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '../spotify-config'; hello, this is a change
-export const SPOTIFY_CLIENT_ID = 'your-client-id';
-export const SPOTIFY_CLIENT_SECRET = 'your-client-secret'
